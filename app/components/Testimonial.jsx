2bd6ca65277429fb058d22c98688219b0486f85d
faizel1 <faizelakmel@gmail.com>
2023-11-08 14:16:10 +0300

    build fix

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -76,7 +76,7 @@ const Testimonial = () => {
             <img src="/img/testimonial1.png" alt="" className='  rounded-t-md ' />
           </div> */}
           <div className="flex flex-col items-center justify-center">
-            <p className='max-w-[20rem] text-center px-8 py-5' >I was blown away by the amazing work done by Nobel Nebil's team! The epoxy flooring they installed in my garage not only looks fantastic, but it's also incredibly durable and easy to clean. I couldn &apos;t be happier with the result, and I would highly recommend their services to anyone looking for top-quality flooring.</p>
+            <p className='max-w-[20rem] text-center px-8 py-5' >I was blown away by the amazing work done by Nobel Nebil&apos;s team! The epoxy flooring they installed in my garage not only looks fantastic, but it&apos;s also incredibly durable and easy to clean. I couldn &apos;t be happier with the result, and I would highly recommend their services to anyone looking for top-quality flooring.</p>
             <h2 className='text-xl font-bold' ><span className='mt-20'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px] mr-1'>-</span>  Yafet Worku</h2>
           </div>
         </div></SwiperSlide>
@@ -87,7 +87,7 @@ const Testimonial = () => {
             <img src="/img/testimonial2.png" alt="" className=' rounded-t-md ' />
           </div> */}
           <div className="flex flex-col items-center justify-center">
-            <p className='max-w-[20rem] text-center px-8 py-5' >I was a bit hesitant to try epoxy flooring at first, but Nobel Nebil's team made the process incredibly easy and stress-free. They were professional, courteous, and efficient, and the end result was absolutely stunning! My floors are now the envy of all my neighbors, and I couldn't be happier with the service I received.</p>
+            <p className='max-w-[20rem] text-center px-8 py-5' >I was a bit hesitant to try epoxy flooring at first, but Nobel Nebil&apos;s team made the process incredibly easy and stress-free. They were professional, courteous, and efficient, and the end result was absolutely stunning! My floors are now the envy of all my neighbors, and I couldn&apos;t be happier with the service I received.</p>
             <h2 className='text-xl font-bold' ><span className='mt-20'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px] mr-1'>-</span> Fitsum Tsegaye</h2>
           </div>
         </div></SwiperSlide>
@@ -97,7 +97,7 @@ const Testimonial = () => {
             <img src="/img/testimonial3.png" alt="" className=' rounded-t-md' />
           </div> */}
           <div className="flex flex-col items-center justify-center">
-            <p className='max-w-[20rem] text-center px-8 py-5' >Nobel Nebil's team exceeded all my expectations with their epoxy flooring services. From the initial consultation to the final walkthrough, they were attentive to my needs and preferences, and worked tirelessly to ensure that everything was done to my satisfaction. I would highly recommend them to anyone looking for high-quality, affordable flooring solutions.</p>
+            <p className='max-w-[20rem] text-center px-8 py-5' >Nobel Nebil&apos;s team exceeded all my expectations with their epoxy flooring services. From the initial consultation to the final walkthrough, they were attentive to my needs and preferences, and worked tirelessly to ensure that everything was done to my satisfaction. I would highly recommend them to anyone looking for high-quality, affordable flooring solutions.</p>
             <h2 className='text-xl font-bold' ><span className='mt-20'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px] mr-1'>-</span> Nahom Haileselasse</h2>
           </div>
         </div></SwiperSlide>
@@ -107,7 +107,7 @@ const Testimonial = () => {
             <img src="/img/testimonial1.png" alt="" className='rounded-t-md ' />
           </div> */}
           <div className="flex flex-col items-center justify-center">
-            <p className='max-w-[20rem] text-center px-8 py-5' >I was impressed by the level of expertise and attention to detail that Nobel Nebil's team brought to my epoxy flooring project. They took the time to explain the process to me and answer all my questions, and the end result was truly stunning. I would definitely use their services again and recommend them to anyone looking for top-quality flooring solutions.</p>
+            <p className='max-w-[20rem] text-center px-8 py-5' >I was impressed by the level of expertise and attention to detail that Nobel Nebil&apos;s team brought to my epoxy flooring project. They took the time to explain the process to me and answer all my questions, and the end result was truly stunning. I would definitely use their services again and recommend them to anyone looking for top-quality flooring solutions.</p>
             <h2 className='text-xl font-bold' ><span className='mt-20'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px] mr-1'>-</span>  Woldemariam Tesfaye</h2>
           </div>
         </div></SwiperSlide>
@@ -117,7 +117,7 @@ const Testimonial = () => {
             <img src="/img/testimonial2.png" alt="" className='rounded-t-md ' />
           </div> */}
           <div className="flex flex-col items-center justify-center">
-            <p className='max-w-[20rem] text-center px-8 py-5' >Nobel Nebil's team not only provided exceptional epoxy flooring services, but also went above and beyond to ensure that I was completely satisfied with the end result. They were friendly, professional, and responsive throughout the entire process, and I couldn't be happier with the finished product. I would definitely recommend them to anyone looking for high-quality, durable flooring solutions.</p>
+            <p className='max-w-[20rem] text-center px-8 py-5' >Nobel Nebil&apos;s team not only provided exceptional epoxy flooring services, but also went above and beyond to ensure that I was completely satisfied with the end result. They were friendly, professional, and responsive throughout the entire process, and I couldn&apos;t be happier with the finished product. I would definitely recommend them to anyone looking for high-quality, durable flooring solutions.</p>
             <h2 className='text-xl font-bold' ><span className='mt-20'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px]'>-</span><span className='ml-[-2px] mr-1'>-</span>Kidus Masresha</h2>
           </div>
         </div></SwiperSlide>
