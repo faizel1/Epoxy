7380b8596fe3bda7b34aff26887b8c0e14a24052
faizel1 <faizelakmel@gmail.com>
2023-11-22 16:53:04 +0300

    font size upadte

--- app/components/serviceCard.tsx
+++ app/components/serviceCard.tsx
@@ -6,9 +6,9 @@ const ServiceCard = ({ image = "", title = "", desc=""  }:any) => {
     return (
         <motion.div
   
-        className="flex flex-col bg-white rounded-[20px] gap-7 md:gap-10 px-10 py-7 md:py-14">
+        className="flex flex-col bg-white rounded-[20px] gap-5 md:gap-10 px-10 py-7 md:py-14">
             {/* <Image src={image} alt="image" width={30} height={30} /> */}
-            <h2 className='text-2xl font-semibold text-[#F58A07] capitalize' >{title}</h2>
+            <h2 className='text-[26px] font-extrabold text-[#F58A07] capitalize flex mx-auto' >{title}</h2>
             <p className='text-[18px] text-[#5B5B5B]' >{desc}</p>
 
 
